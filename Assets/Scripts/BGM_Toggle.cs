a53b7d7b4d560eb544b308a443df6c6e77e4d29c
JungHun <yjh4713@gmail.com>
2019-11-13 18:04:44 +0900

    SomeThing

---
+++ Assets/Scripts/BGM_Toggle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGM_Toggle : MonoBehaviour
+{
+    [SerializeField] AudioSource MainBGM;
+
+
+    bool off;
+
+    private void Start()
+    {
+        off = false;
+    }
+    public void BGMSetting()
+    {
+        if (off == false)
+        {
+            MainBGM.Stop();
+            off = true;
+        }
+        else
+        {
+            MainBGM.Play();
+            off = false;
+        }
+    }
+}
---
+++ Assets/Scripts/WindowClose.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WindowClose : MonoBehaviour
+{
+    [SerializeField] GameObject Window;
+
+    public void OnClickExit()
+    {
+        Window.SetActive(false);
+    }
+}
